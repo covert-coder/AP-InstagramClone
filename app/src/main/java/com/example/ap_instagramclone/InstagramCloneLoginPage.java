30b363a53d5acdee1a1c46f81089a68afed5913a
covert_coder <rgeeinstead@gmail.com>
2020-03-24 16:40:12 -0600

    Instagram clone setup for login and signup pre deletion of old mainactivity and other files from earlier lessons not needed in this app

--- app/src/main/java/com/example/ap_instagramclone/InstagramCloneLoginPage.java
+++ app/src/main/java/com/example/ap_instagramclone/InstagramCloneLoginPage.java
@@ -5,18 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class InstagramCloneLoginPage extends AppCompatActivity {
+
+    private EditText txtEmailOnfile;
+    private EditText txtPassOnfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instagram_clone_login_page);
 
+        txtEmailOnfile = findViewById(R.id.txtEmailExist);
+        txtPassOnfile = findViewById(R.id.txtPassExist);
+
+
         findViewById(R.id.btnLoginExisting).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                ParseUser.logInInBackground(txtEmailOnfile.getText().toString(),
+                        txtPassOnfile.getText().toString(), new LogInCallback() {
+                            @Override
+                            public void done(ParseUser user, ParseException e) {
+                                if(user!=null && e==null){
+                                    Toast.makeText(InstagramCloneLoginPage.this, "your login was successful "+user.get("username")
+                                            , Toast.LENGTH_LONG).show();
+                                    Intent welcomeIntent = new Intent(InstagramCloneLoginPage.this, WelcomeActivity.class);
+                                    startActivity(welcomeIntent);
+                                }else{
+                                    Toast.makeText(InstagramCloneLoginPage.this, "your login failed: "+ e.getMessage(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
             }
         });
 
--- app/src/main/java/com/example/ap_instagramclone/InstagramCloneMainPage.java
+++ app/src/main/java/com/example/ap_instagramclone/InstagramCloneMainPage.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
 
 public class InstagramCloneMainPage extends AppCompatActivity {
-    private EditText mLoginInstag;
-    private EditText mSignupInstag;
+    private EditText mPasswordInstag;
+    private EditText mUserNameInstag;
     private EditText mEmailInstag;
 
     @Override
@@ -18,13 +25,27 @@ public class InstagramCloneMainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instagram_clone_main_page);
 
+        ParseUser.logOut();
+
+        mUserNameInstag = findViewById(R.id.txtUserInstagram);
+        mEmailInstag = findViewById(R.id.txtEmailInstagram);
+        mPasswordInstag = findViewById(R.id.txtPassInstagram);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+        ParseObject mInstagram = new ParseObject("InstagramClone");
+        mInstagram.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                Log.i("myTag","the new class InstagramClone, was created");
+            }
+        });
+
         findViewById(R.id.btnLoginInstagram).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intentLogin = new Intent(InstagramCloneMainPage.this,
                         InstagramCloneLoginPage.class);
                 startActivity(intentLogin);
-
             }
         });
 
@@ -32,9 +53,28 @@ public class InstagramCloneMainPage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                ParseUser appUser = new ParseUser();
+                appUser.setUsername(mUserNameInstag.getText().toString());
+                appUser.setPassword(mPasswordInstag.getText().toString());
+                appUser.setEmail(mEmailInstag.getText().toString());
+
+                appUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(InstagramCloneMainPage.this, "your password and login were " +
+                                    "set successfully;", Toast.LENGTH_LONG).show();
+
+                            Intent welcomeIntent = new Intent(InstagramCloneMainPage.this, WelcomeActivity.class);
+                            startActivity(welcomeIntent);
+                        }
+                        else{
+                            Toast.makeText(InstagramCloneMainPage.this, "your password and login were " +
+                                    "not successful;"+e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         });
-
-
     }
 }
