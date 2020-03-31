ce7ef4f25e4d42e242eb652d92c35f27412961ed
covert_coder <rgeeinstead@gmail.com>
2020-03-31 15:35:33 -0600

    Instagram clone update, included AsyncTask method to take resource intensive processes off of the main thread, thereby allowing UI elements to run without

--- app/src/main/java/com/example/ap_instagramclone/SharePictureTab.java
+++ app/src/main/java/com/example/ap_instagramclone/SharePictureTab.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -41,66 +42,33 @@ public class SharePictureTab extends Fragment implements View.OnClickListener {
     private ParseObject parseObject;
     private ParseFile mParseFile;
     private Bitmap receivedImageBitmap;
+
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
 
             case R.id.imgPlace:
                 // first this if stmt checks to see if permission has been previously granted
                 // if it has, this is stored in,
-            if(android.os.Build.VERSION.SDK_INT >= 23 && ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()),
-                    Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                if (android.os.Build.VERSION.SDK_INT >= 23 && ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()),
+                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
-                // body of if stmt
-                requestPermissions(new String[]{
-                        Manifest.permission.READ_EXTERNAL_STORAGE
-                        }
-                        , 1000); // we have created this code number, which will be stored int
-                                            // the manifest as a unique identifier of this READ_EXTERNAL_STORAGE request
-            } else{
-                getChosenImage();
-            }
+                    // body of if stmt
+                    requestPermissions(new String[]{
+                                    Manifest.permission.READ_EXTERNAL_STORAGE
+                            }
+                            , 1000); // we have created this code number, which will be stored int
+                    // the manifest as a unique identifier of this READ_EXTERNAL_STORAGE request
+                } else {
+                    getChosenImage();
+                }
                 break;
 
             case R.id.btnShare:
+                mProgressBar.setVisibility(View.VISIBLE);
+                Log.i("myTag", "post image was pressed");
 
-                Log.i("myTag","post image was pressed");
-
-                if(receivedImageBitmap != null){
-
-                    // check to see if the description was filled out. An empty string will indicate it wasn't.
-                    if(mEditImgDescription.getText().toString().equals("")){
-                        Toast.makeText(getContext(), "You need to add a description before submission", Toast.LENGTH_LONG).show();
-                    }else{
-                        mProgressBar.setVisibility(View.VISIBLE);
-
-                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                        receivedImageBitmap.compress(Bitmap.CompressFormat.PNG,100,byteArrayOutputStream);
-                        byte[] bytes = byteArrayOutputStream.toByteArray();
-                        ParseFile parseFile = new ParseFile("pic.png", bytes);
-                        parseObject = new ParseObject("Photo");
-                        parseObject.put("picture", parseFile);
-                        parseObject.put("image_des", mEditImgDescription.getText().toString());
-                        parseObject.put("username", ParseUser.getCurrentUser().getUsername());
-
-                        parseObject.saveInBackground(new SaveCallback() {
-                            @Override
-                            public void done(ParseException e) {
-                                if(e==null){
-                                    //Toast.makeText(getContext(), "Done!!", Toast.LENGTH_SHORT).show();
-                                    Toast.makeText(getContext(), "Your image has been posted to the server", Toast.LENGTH_LONG).show();
-
-                                    mProgressBar.setVisibility(View.GONE);
-                                }else{
-                                    Toast.makeText(getContext(), "Error :"+e.getMessage(), Toast.LENGTH_LONG).show();
-                                    Log.i("myTag", "the error is; "+e.getMessage());
-                                }
-                            }
-                        });
-                    }
-                }else
-                    Toast.makeText(getContext(), "You must select an image by clicking on the image " +
-                            "above before posting your image", Toast.LENGTH_LONG).show();
+                AsyncTask progressBar = new progressBar().execute();
                 break;
         }
     }
@@ -168,6 +136,68 @@ public class SharePictureTab extends Fragment implements View.OnClickListener {
                     Log.i("myTag", "the exception is; " + e.getMessage());
                 }
             }
+        }
+    }
+
+    // AsyncTask to isolate resource intensive task from main thread, thereby allowing progress bar to run
+    // immediately and to prevent crashes should the "post image" button be pressed again or should the
+    // screen be rotated (to landscape)
+    private class progressBar extends AsyncTask<String, Void, ParseObject> {
+
+        @Override
+        protected ParseObject doInBackground(String... strings) {
+
+            if (receivedImageBitmap != null) {
+
+                // check to see if the description was filled out. An empty string will indicate it wasn't.
+                if (mEditImgDescription.getText().toString().equals("")) {
+                    //Toast.makeText(getContext(), "You need to add a description before submission", Toast.LENGTH_LONG).show();
+                } else {
+
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    receivedImageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                    byte[] bytes = byteArrayOutputStream.toByteArray();
+                    ParseFile parseFile = new ParseFile("pic.png", bytes);
+                    parseObject = new ParseObject("Photo");
+                    parseObject.put("picture", parseFile);
+                    parseObject.put("image_des", mEditImgDescription.getText().toString());
+                    parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+
+                }
+            } else {
+                //Toast.makeText(getContext(), "You must select an image by clicking on the image " +
+                        //"above before posting your image", Toast.LENGTH_LONG).show();
+            }
+            return parseObject;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(ParseObject s) {
+            super.onPostExecute(s);
+            parseObject.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        //Toast.makeText(getContext(), "Done!!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Your image has been posted to the server", Toast.LENGTH_LONG).show();
+
+                        mProgressBar.setVisibility(View.GONE);
+                    } else {
+                        Toast.makeText(getContext(), "Error :" + e.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.i("myTag", "the error is; " + e.getMessage());
+                    }
+                }
+            });
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
         }
     }
 }
